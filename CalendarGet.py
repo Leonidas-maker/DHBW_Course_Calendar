7eaee14d40355b5ec60971978aba72c0deafb8e7
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2022-11-23 22:46:13 +0100

    add CalendarGet

---
+++ CalendarGet.py
@@ -0,0 +1,55 @@
+# imports
+from ics import Calendar
+import requests
+import os
+from datetime import datetime
+import time
+import hashlib
+
+# url to download ics file
+url = "http://vorlesungsplan.dhbw-mannheim.de/ical.php?uid=8537001"
+
+# save content in doc
+doc = requests.get(url)
+
+# gets todays date and new format
+now = datetime.now()
+
+day = now.strftime("%d_%m_%Y")
+print(day)
+
+time = now.strftime("%H_%M_%S")
+print(time)
+
+#creates day folder
+try:
+    os.mkdir("src/" + day)
+    print("Directory " + day + " Created")
+except FileExistsError:
+    print("Directory " + day + " already exists")
+
+#creates time folder
+try:
+    os.mkdir("src/" + day + "/" + time)
+    print("Directory " + time + " Created")
+except FileExistsError:
+    print("Directory " + time + " already exists")
+
+# creates file "DHBW_cal.ics" in folder src
+open("src/" + day + "/" + time + "/DHBW_cal.ics", "wb").write(doc.content)
+
+file = ("src/" + day + "/" + time + "/DHBW_cal.ics")
+BLOCK_SIZE = 65536
+
+file_hash = hashlib.sha256()
+with open(file, "rb") as f:
+    fb = f.read(BLOCK_SIZE)
+    while len(fb) > 0:
+        file_hash.update(fb)
+        fb = f.read(BLOCK_SIZE)
+
+hash = file_hash.digest
+print(hash)
+
+open("src/" + day + "/" + time + "/DHBW_cal_hash.txt", "w").write(str(file_hash.copy))
+print(file_hash.hexdigest())
